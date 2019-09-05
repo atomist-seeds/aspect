5dae38d37d980f795aeca7386a9b4e36a2f57e38
Christian Dupuis <cd@atomist.com>
2019-09-05 19:19:56 +0200

    Initial commit

---
+++ index.ts
@@ -0,0 +1,53 @@
+import { PushImpact } from "@atomist/sdm";
+import { configure } from "@atomist/sdm-core";
+import { DeliveryGoals } from "@atomist/sdm-core/lib/machine/configure";
+import { aspectSupport } from "@atomist/sdm-pack-aspect";
+import {
+    RebaseFailure,
+    RebaseStrategy,
+} from "@atomist/sdm-pack-fingerprint";
+import { LicenseAspect } from "./lib/license";
+
+// This SDM only has a single PushImpact goal which is used
+// to run your aspects on Git pushes
+interface AnalyzeGoals extends DeliveryGoals {
+
+    pushImpact: PushImpact;
+}
+
+// Main entry point into the SDM
+export const configuration = configure<AnalyzeGoals>(async sdm => {
+
+    // This creates and configures the goal instance
+    const goals = await sdm.createGoals(async () => ({ pushImpact: new PushImpact() }));
+
+    // This installs the required extension pack into the SDM
+    // to run aspects and expose the local web ui for testing
+    sdm.addExtensionPacks(
+        aspectSupport({
+
+            // Pass the aspects you want to run in this SDM
+            aspects: LicenseAspect,
+
+            // Pass the PushImpact goal into the aspect support for it
+            // to get configured
+            goals,
+
+            // Configure how existing branches should be rebased
+            // during aspect apply executions
+            rebase: {
+                rebase: true,
+                rebaseStrategy: RebaseStrategy.Ours,
+                onRebaseFailure: RebaseFailure.DeleteBranch,
+            },
+        }),
+    );
+
+    // Return a signal goal set to run the push impact goal
+    // on any push
+    return {
+        analyze: {
+            goals: goals.pushImpact,
+        },
+    };
+});
---
+++ lib/license.ts
@@ -0,0 +1,88 @@
+import {
+    HttpMethod,
+    ProjectOperationCredentials,
+    TokenCredentials,
+} from "@atomist/automation-client";
+import { isTokenCredentials } from "@atomist/automation-client/lib/operations/common/ProjectOperationCredentials";
+import {
+    Aspect,
+    sha256,
+} from "@atomist/sdm-pack-fingerprint";
+
+interface LicenseData {
+    key: string;
+    name: string;
+    spdx: string;
+    url: string;
+}
+
+export const LicenseAspect: Aspect<LicenseData> = {
+    name: "gh-license",
+    displayName: "License",
+    extract: async (p, pli) => {
+        const url = `https://api.github.com/repos/${p.id.owner}/${p.id.repo}/license`;
+        const client = pli.configuration.http.client.factory.create(url);
+
+        try {
+            const data = (await client.exchange<any>(url, {
+                method: HttpMethod.Get,
+                headers: headers(pli),
+                retry: {
+                    retries: 0,
+                },
+            })).body;
+            const license = {
+                key: data.license.key,
+                name: data.license.name,
+                spdx: data.license.spdx_id,
+                url: data.license.url,
+            };
+            return {
+                type: "gh-license",
+                name: "gh-license",
+                abbreviation: "lic",
+                version: "0.0.1",
+                data: license,
+                sha: sha256(JSON.stringify(license)),
+            };
+        } catch (e) {
+            return [];
+        }
+    },
+    apply: async (p, papi) => {
+        const fp = papi.parameters.fp;
+        const key = fp.data.key;
+        const url = `https://api.github.com/licenses`;
+        const licenses = await papi.configuration.http.client.factory.create(url)
+            .exchange<Array<{ key: string, url: string }>>(
+                url,
+                {
+                    method: HttpMethod.Get,
+                    headers: headers(papi),
+                });
+
+        const license = licenses.body.find(l => l.key === key);
+        if (!!license) {
+            const licenseText = await papi.configuration.http.client.factory.create(license.url)
+                .exchange<{ body: string }>(license.url, { method: HttpMethod.Get });
+
+            const licenseFile = await p.getFile("LICENSE");
+            if (!!licenseFile) {
+                await licenseFile.setContent(licenseText.body.body);
+            } else {
+                await p.addFile("LICENSE", licenseText.body.body);
+            }
+        }
+        return p;
+    },
+    toDisplayableFingerprint: fp => fp.data.name,
+    toDisplayableFingerprintName: () => "License",
+};
+
+export function headers(papi: { credentials: ProjectOperationCredentials }): any {
+    if (!!papi.credentials && isTokenCredentials(papi.credentials)) {
+        return { Authorization: `Bearer ${(papi.credentials).token}` };
+    } else {
+        return undefined;
+    }
+}
